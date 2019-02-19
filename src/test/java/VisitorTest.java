603d25aaa82d26003883aeba6fb927975038a1c2
Joe McColl <joe4kool@gmail.com>
2019-02-19 15:27:43 +0000

    setters within visitor class tested and passing

--- src/test/java/VisitorTest.java
+++ src/test/java/VisitorTest.java
@@ -32,4 +32,29 @@ public class VisitorTest {
 		assertEquals(40, visitor.getMoney(), 0.01);
 	}
 
+	@Test
+	public void canSetVisitorName(){
+		visitor.setName("Schtevie");
+		assertEquals("Schtevie", visitor.getName());
+	}
+
+	@Test
+	public void canSetVisitorAge(){
+		visitor.setAge(50);
+		assertEquals(50, visitor.getAge());
+	}
+
+	@Test
+	public void canSetVisitorHeight(){
+		visitor.setHeight(173);
+		assertEquals(173, visitor.getHeight());
+	}
+
+	@Test
+	public void canSetVisitorMoney(){
+		visitor.setMoney(100);
+		assertEquals(100, visitor.getMoney(), 0.01);
+	}
+
+
 }
