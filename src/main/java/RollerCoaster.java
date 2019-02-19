9cb7f41806df9504b44956d89815484064794a21
Joe McColl <joe4kool@gmail.com>
2019-02-19 15:59:56 +0000

    added boolean isAllowed interface to rollercoaster, tests pass

--- src/main/java/RollerCoaster.java
+++ src/main/java/RollerCoaster.java
@@ -1,7 +1,15 @@
-public class RollerCoaster extends Attraction{
+public class RollerCoaster extends Attraction implements ISecurity{
 
 	public RollerCoaster(String name) {
 		super(name);
+	}
+
+	public boolean isAllowedTo(Visitor visitor){
+		boolean allowed = true;
+		if (visitor.getAge() < 13 || visitor.getHeight() < 146){
+			allowed = false;
+		}
+		return allowed;
 	}
 
 }
--- src/test/java/RollerCoasterTest.java
+++ src/test/java/RollerCoasterTest.java
@@ -6,14 +6,23 @@ import static org.junit.Assert.assertEquals;
 public class RollerCoasterTest {
 
 	RollerCoaster rollerCoaster;
+	Visitor visitor;
 
 	@Before
 	public void before(){
 		rollerCoaster = new RollerCoaster("Oblivion");
+		visitor = new Visitor("Jon", 28, 175, 40);
 	}
 
 	@Test
 	public void hasName(){
 		assertEquals("Oblivion", rollerCoaster.getName());
 	}
+
+	@Test
+	public void testAllowedOnAttraction(){
+		assertEquals(true, rollerCoaster.isAllowedTo(visitor));
+	}
+
+
 }
